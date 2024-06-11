a917149fb788f6d788657457a227c49ec152cfda
Tran Tuan Anh <anhtthe182190@fpt.edu.vn>
2024-06-11 09:42:23 +0700

    logout function

--- ReviewQuanAn/src/java/service/Logout.java
+++ ReviewQuanAn/src/java/service/Logout.java
@@ -2,26 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package Controller;
+package service;
 
-import entity.Blog;
+import entity.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.Vector;
-
-import dao.DAOBlog;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
- * @author lam1
+ * @author ACER
  */
-@WebServlet(name = "Home", urlPatterns = {"/home"})
-public class Home extends HttpServlet {
+public class Logout extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -35,18 +31,16 @@ public class Home extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Home</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Home at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        HttpSession session = request.getSession(true);
+//        User user = (User) session.getAttribute("User");
+//        if(user == null){
+//            user = (User)session.getAttribute("Admin");
+//            if(user == null){
+//                response.sendRedirect("home");
+//            }
+//        }
+        response.sendRedirect("home");
+        session.invalidate();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -61,17 +55,7 @@ public class Home extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        request.setCharacterEncoding("UTF-8");
-        DAOBlog dao = new DAOBlog();
-        try {
-            Vector<Blog> list = dao.getAll();
-            request.setAttribute("list", list);
-            request.getRequestDispatcher("HomePage.jsp").forward(request, response);
-        } catch (Exception e) {
-             
-        }
-
+        processRequest(request, response);
     }
 
     /**
@@ -85,16 +69,7 @@ public class Home extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        request.setCharacterEncoding("UTF-8");
-        DAOBlog dao = new DAOBlog();
-        try {
-            Vector<Blog> list = dao.getAll();
-            request.setAttribute("list", list);
-            request.getRequestDispatcher("HomePage.jsp").forward(request, response);
-        } catch (Exception e) {
-             
-        }
+        processRequest(request, response);
     }
 
     /**
