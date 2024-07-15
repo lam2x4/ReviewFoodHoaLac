792b7ab755dd7158d247348e8215affd189dae9b
Le Hoang Anh (K18 HL) <anhlhhe186102@fpt.edu.vn>
2024-07-15 18:40:12 +0700

    Readding the User file since it's corrupted

--- ReviewQuanAn/src/java/entity/User.java
+++ ReviewQuanAn/src/java/entity/User.java
@@ -1,8 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package entity;
 
 /**
  *
- * @author AnhTT
+ * @author ADMIN
  */
 public class User {
 
